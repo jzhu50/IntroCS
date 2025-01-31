3e7ee9b887e378d39308f9bb73092f5af0d9aa78
Michelle <jzhu50@stuy.edu>
2025-01-30 20:44:37 -0500

    moved files

--- lab15b/fillmenus.py
+++
@@ -1,6 +0,0 @@
-#loop over list to generate dropdown...
-
-for i in range(5):
-    VARIABLE = "Monster"+str(i)
-    print('''<li><a href="./'''+VARIABLE+'''.html">'''+VARIABLE+'''</a></li>''' )
-    
